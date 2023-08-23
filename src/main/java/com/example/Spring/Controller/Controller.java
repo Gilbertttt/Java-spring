7a478fa045454d3946e5c4a17dda730a3e29c9d7
Gilbertt <umorengilbert17@gmail.com>
2023-08-23 16:02:52 +0100

    Added some dependencies, Set up a validation system Worked with path variables

--- src/main/java/com/example/Spring/Controller/Controller.java
+++ src/main/java/com/example/Spring/Controller/Controller.java
@@ -1,5 +1,8 @@
 package com.example.Spring.Controller;
 
+import com.example.Spring.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,6 +15,9 @@ import java.util.List;
             ArrayList <String> lists = new ArrayList<>(Arrays.asList("Phone", "Laptop", "VGA", "Wifi Router"));
             ArrayList <String> phone = new ArrayList<>(Arrays.asList("Infinix", "Iphone", "Samsung", "Itel"));
             ArrayList <String> Laptop = new ArrayList<>(Arrays.asList("Apple", "Del", "Samsung", "Toshiba"));
+
+            ArrayList <User> users = new ArrayList<User>();
+
 
             @RequestMapping(value = {"/Home-Page", "/"})  // Getting the same page request
             public String homePage() {
@@ -44,8 +50,48 @@ import java.util.List;
             }
 
             @GetMapping(value = "/Electronics/{item}")
-            public ArrayList<String> pathVariable(@PathVariable String item) {
+            public ArrayList<String> multiplePathVariable(@PathVariable String item) {
                 System.out.println(item);
+                if(item.equalsIgnoreCase("phone")) {
+                    return phone;
+                }
+                if (item.equalsIgnoreCase("laptop")) {
+                    return Laptop;
+                }
                 return null;
+
             }
+            @GetMapping("/electronic-pages/{item}")
+            public ResponseEntity<Object> pathVariable(@PathVariable String item) {
+        System.out.println(item);
+        ResponseEntity<Object> responseEntity = new ResponseEntity(item + "Not Found",HttpStatus.NOT_FOUND);
+        if(item.equalsIgnoreCase("phone")) {
+            return ResponseEntity.ok(phone);
+        }
+        if (item.equalsIgnoreCase("laptop")) {
+            return ResponseEntity.ok(Laptop);
+        }
+        return null;
+
+        }
+
+          @PostMapping("/register")
+          public ResponseEntity<?> register(@RequestBody User user) {
+              System.out.println(user);
+
+              for (User currentUser:users){
+                  if (user.getPhone().equals(currentUser.getPhone())) {
+                      return new ResponseEntity<>("Phone Number Already used",HttpStatus.BAD_REQUEST);
+                  }
+              }
+              int id = users.size() + 1;
+              user.setId(id);
+              users.add(user);
+                return ResponseEntity.ok("Registered Successfully");
+          }
+
+          @GetMapping ("/users")
+    public ResponseEntity<?> user (){
+                return ResponseEntity.ok(users);
+          }
         }
